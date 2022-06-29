78d2c9a8397a8729c532573f7994f621f752762e
Waleed <62436128+WaliiidAls@users.noreply.github.com>
2022-06-29 18:07:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// عرف المصفوفة هنا
+
+function addOrder() {
+  let name = document.getElementById("name").value;
+  let id = document.getElementById("id").value;
+  let phone = document.getElementById("phone").value;
+  let order = document.getElementById("order").value;
+  
+}
+
+function load() {
+  let container = document.getElementById("container");
+  
+}
