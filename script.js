eaa125772f47da7a55d1b05d3f8ddfc52d740338
zainab <zainabdashty2@gmail.com>
2022-08-24 19:18:15 +0300

    done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
+let orders = [   ]
 
 
 function addOrder() {
@@ -6,18 +7,29 @@ function addOrder() {
   let id = document.getElementById("id").value;
   let phone = document.getElementById("phone").value;
   let order = document.getElementById("order").value;
+
+
   
   // أنشئ كائن newOrder
-  
+  let neworder = {
+    name, id, phone, order,
+  }
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(neworder)
   // طبق الخطوة 7 تحت هذا الكومنت
+  load()
   
 }
 
 function load() {
   let container = document.getElementById("container");
-  container.innerHTML = "";
-  // طبّق الخطوة 6 تحت هذا الكومنت
 
+  // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach(item => {
+    container.innerHTML =  ` <div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div> `
+    
+  });
 }
+
+
+
